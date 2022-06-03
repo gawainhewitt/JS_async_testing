2bd1dd4fc8d28a8c2455ee97626991681ff56c5c
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-03 23:21:31 +0100

    re-written class using async style

--- gitHubApi.js
+++ gitHubApi.js
@@ -1,13 +1,29 @@
-async function convert(repoName, callback) {
-  try {
-    const result = await fetch(
-      `https://api.github.com/repos/${repoName}`
-    );
-    const data = await result.json();
-    callback(data);
-  } catch (e) {
-    return null;
+class GitHubApi {  
+  async getRepoInfo(repoName, callback) {
+    try {
+      const result = await fetch(
+        `https://api.github.com/repos/${repoName}`
+      );
+      const data = await result.json();
+      console.log(data);
+      callback(data);
+    } catch (error) {
+      return null;
+    }
   }
 }
 
-module.exports = GithubApi;
+// class GitHubApi {
+//   getRepoInfo(repoName, callback) {
+//     fetch('https://api.github.com/repos/' + repoName)
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//       });
+//   }
+// }
+
+
+module.exports = GitHubApi;
+
+
